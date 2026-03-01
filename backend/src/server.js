ca4a19b54b4055823a2fa034ba168efcb0daa34e
Khushal <Khushal.badaya@gmail.com>
2026-03-01 15:53:28 +0530

    ready for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,26 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
 
 import authRoutes from "./routes/auth.routes.js"
 import messageRoutes from "./routes/message.routes.js"
 dotenv.config();
 const app = express();
 
+const __dirname = path.resolve();
 const PORT= process.env.PORT || 3000
 
 app.use("/api/auth", authRoutes);
 app.use("/api/auth", messageRoutes);
+
+
+// made for development 
+if ( process.env.NODE_ENV=='production'){
+    app.use(express.static(path.join(__dirname,"../fronent/dist")));
+
+    app.get("*",(_,res)=>{
+        res.sendFile(path.join(__dirname,"../fronend","dist","index.html"));
+    });
+}
+
 app.listen(PORT,()=>console.log("Sever is running on port : "+ PORT));
